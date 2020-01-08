92f4c6df64eab2fe0504821f03e5b37933d7ac13
Jordi Gimenez <jordi.gimenez@quantion.com>
2020-01-08 21:22:07 +0100

    Incluir los trasteros de cada contrato en la respuesta

--- customerportalapi.Entities/Contract.cs
+++ customerportalapi.Entities/Contract.cs
@@ -10,8 +10,24 @@ namespace customerportalapi.Entities
 
         public string Store { get; set; }
 
+        public string Currency { get; set; }
+
+        public string Price { get; set; }
+
+        public string Vat { get; set; }
+
+        public string ReservationFee { get; set; }
+
         public string ContractDate { get; set; }
 
-        public int ContractStatus { get; set; }
+        public string FirstPaymentDate { get; set; }
+
+        public string FirstPayment { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public string Customer { get; set; }
+
+        public Unit Unit { get; set; }
     }
 }
---
+++ customerportalapi.Entities/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customerportalapi.Entities
+{
+    public class Unit
+    {
+        public string UnitName { get; set; }
+
+        public string Size { get; set; }
+
+        public string UnitCategory { get; set; }
+
+        public string Subtype { get; set; }
+
+        public string Height { get; set; }
+
+        public string Width { get; set; }
+
+        public string Depth { get; set; }
+    }
+    
+}
