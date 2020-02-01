33f67fb7332b0524e33f8ec3bc93c10cce6d15cf
Jordi Gimenez <jordi.gimenez@quantion.com>
2020-02-01 19:20:00 +0100

    Ejemplo de codificar y decodificar jwt token y api de ejemplo con authorize attribute

---
+++ customerportalapi/Security/AuthorizeTokenAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customerportalapi.Security
+{
+    public class AuthorizeTokenAttribute : TypeFilterAttribute
+    {
+        //public AuthorizeTokenAttribute(IHostingEnvironment environment, IConfiguration config, ILog log) : base(typeof(AuthorizeTokenFilter))
+        public AuthorizeTokenAttribute() : base(typeof(AuthorizeTokenFilter))
+        {
+
+        }
+
+    }
+}
---
+++ customerportalapi/Security/JwtTokenHelper.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace customerportalapi.Security
+{
+    public static class JwtTokenHelper
+    {
+        public static string GenerateToken(IConfiguration config, string username, string role, string email, DateTime expirationDate)
+        {
+            var symmetricKey = Convert.FromBase64String(config["Identity:Credential:ClientSecret"]);
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                        {
+                            new Claim(ClaimTypes.NameIdentifier, username),
+                            new Claim(ClaimTypes.Role, role),
+                            new Claim(ClaimTypes.Email, email),
+                            new Claim(ClaimTypes.Expiration, expirationDate.ToString())
+                        }),
+
+                Expires = expirationDate,
+
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(symmetricKey), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var stoken = tokenHandler.CreateToken(tokenDescriptor);
+            var token = tokenHandler.WriteToken(stoken);
+
+            return token;
+        }
+
+        public static ClaimsPrincipal GetPrincipal(string token, IConfiguration config)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwtToken = tokenHandler.ReadToken(token) as JwtSecurityToken;
+
+            if (jwtToken == null)
+                return null;
+
+            bool validateLifetime = true;
+#if DEBUG
+            validateLifetime = false;
+#endif
+
+            var symmetricKey = Convert.FromBase64String(config["Identity:Credential:ClientSecret"]);
+            var validationParameters = new TokenValidationParameters()
+            {
+                RequireExpirationTime = true,
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                IssuerSigningKey = new SymmetricSecurityKey(symmetricKey),
+                ValidateLifetime = validateLifetime
+            };
+
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, validationParameters, out securityToken);
+
+            return principal;
+        }
+    }
+}
