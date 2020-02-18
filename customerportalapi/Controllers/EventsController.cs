7bd48c6dde8c96b69fa7f66f79a2bfe5a204b5af
Júlia Alsina <julia.alsina@quantion.com>
2020-02-18 14:59:23 +0100

    task: added notification endpoint (empty)

---
+++ customerportalapi/Controllers/EventsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using customerportalapi.Entities;
+
+namespace customerportalapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        private readonly ILogger<EventsController> _logger;
+
+        public EventsController(ILogger<EventsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult SignatureStatus([FromBody] SignatureStatus value)
+        {
+            try
+            {
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                throw;
+            }
+        }
+    }
+}
