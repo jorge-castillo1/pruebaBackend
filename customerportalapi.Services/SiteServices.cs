44fc2c6656a0b49a2bdf80bbae8b95d2d180c6c0
Christian Garcia <christian.garcia@quantion.com>
2020-03-09 11:20:41 +0000

    15882 - [Back] Utilizar SmContract en lugar de ContractNumber para recuperar los datos del contrato

--- customerportalapi.Services/SiteServices.cs
+++ customerportalapi.Services/SiteServices.cs
@@ -75,7 +75,8 @@ namespace customerportalapi.Services
                     contract.StoreCode = contract.StoreData.StoreCode;
                     contract.AccessType = contract.StoreData.AccessType;
                     contract.MapLink = contract.StoreData.MapLink;
-                    SMContract contractSM = await _contractSMRepository.GetAccessCodeAsync(contract.ContractNumber);
+
+                    SMContract contractSM = await _contractSMRepository.GetAccessCodeAsync(contract.SmContractCode);
                     contract.TimeZone = contractSM.Timezone;
                     contract.StoreData = null;
                     site.Contracts.Add(contract);
@@ -160,10 +161,10 @@ namespace customerportalapi.Services
             if (entity.AccesToken == null) {
                 throw new ServiceException("Password not valid", HttpStatusCode.BadRequest);
             }
+            Contract contract = await _contractRepository.GetContractAsync(contractId);
+            SMContract smContract = await _contractSMRepository.GetAccessCodeAsync(contract.SmContractCode);
 
-            SMContract contract = await _contractSMRepository.GetAccessCodeAsync(contractId);
-
-            entity.Password = contract.Password;
+            entity.Password = smContract.Password;
             entity.ContractId = contractId;
 
             return entity;
