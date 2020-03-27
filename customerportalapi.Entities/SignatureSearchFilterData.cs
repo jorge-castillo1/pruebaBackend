eb70a03c383804c6970f6d7f02f970990e41f021
Jordi Giménez <jordi.gimenez@quantion.com>
2020-03-27 13:32:52 +0000

    16341

---
+++ customerportalapi.Entities/SignatureSearchFilterData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace customerportalapi.Entities
+{
+    public class SignatureSearchFilterData
+    {
+        public string SignatureId { get; set; }
+        public string Store { get; set; }
+        public string DocumentType { get; set; }
+        public string UnitIdentification { get; set; }
+        public string UnitType { get; set; }
+        public string UserIdentification { get; set; }
+        public string RecipientEmail { get; set; }
+        public string RecipientPhone { get; set; }
+        public string SignatureChannel { get; set; }
+        public string SignatureType { get; set; }
+        public string SignatureDateFrom { get; set; }
+        public string SignatureDateTo { get; set; }
+    }
+}
--- customerportalapi.Repositories/interfaces/ISignatureRepository.cs
+++ customerportalapi.Repositories/interfaces/ISignatureRepository.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Net.Http;
+using customerportalapi.Entities;
+using System.Collections.Generic;
 
 namespace customerportalapi.Repositories.interfaces
 {
@@ -8,5 +10,6 @@ namespace customerportalapi.Repositories.interfaces
     {
         Task<Guid> CreateSignature(MultipartFormDataContent form);
         Task<bool> CancelSignature(string id);
+        Task<List<SignatureProcess>> SearchSignaturesAsync(SignatureSearchFilter filter);
     }
 }
