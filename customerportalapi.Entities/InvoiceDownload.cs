ef91c491b9da3a8a6790f575fd18a521bbdd3cad
Jordi Piella <jordi.piella@quantion.com>
2020-04-15 15:31:33 +0200

    task: add invoice download method

---
+++ customerportalapi.Entities/InvoiceDownload.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace customerportalapi.Entities
+{
+    public class InvoiceDownload
+    {
+        public string StoreCode { get; set; }
+
+        public string InvoiceNumber { get; set; }
+
+        public string Username { get; set; }
+    }
+
+}
