04c693c157939029b13aa0de3b296d2ed0cbe4ff
Jordi Gimenez <jordi.gimenez@quantion.com>
2020-11-12 11:25:54 +0100

    nuget config

---
+++ Quantion.MongoDbLogger/MongoDbLoggerProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace Quantion.MongoDbLogger
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [ProviderAlias("MongoDbLogger")]
+    public class MongoDbLoggerProvider : ILoggerProvider
+    {
+        private readonly ConcurrentDictionary<string, MongoDbLogger> _loggers = new ConcurrentDictionary<string, MongoDbLogger>();
+        private readonly MongoDbLoggerConfiguration _config;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        public MongoDbLoggerProvider(MongoDbLoggerConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="categoryName"></param>
+        /// <returns></returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return _loggers.GetOrAdd(categoryName, name => new MongoDbLogger(name, _config));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            _loggers.Clear();
+        }
+    }
+}
