6afd424ed441a0ff6cfe0343343e84ceabe33367
Jordi Piella <jordi.piella@quantion.com>
2020-11-25 09:21:36 +0000

    US17993 - Opción de cambiar código de acceso (interacción con Space Manager)

---
+++ customerportalapi.Entities/SubContract.cs
@@ -0,0 +1,10 @@
+﻿namespace customerportalapi.Entities
+{
+    public class SubContract
+    {
+        public string Contractnumber { get; set; }
+        public string SubContractId { get; set; }
+        public string Unitid { get; set; }
+
+    }
+}
