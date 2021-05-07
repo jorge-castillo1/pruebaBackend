36a75d473e48d0dbf3d66089c7d861b228eed671
Christian Garcia PC-TPT2 <christian.garcia@quantion.com>
2021-05-07 11:27:20 +0200

    feat: check Feature before use email welcome

---
+++ customerportalapi.Repositories/interfaces/IFeatureRepository.cs
@@ -0,0 +1,14 @@
+﻿using customerportalapi.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customerportalapi.Repositories.interfaces
+{
+    public interface IFeatureRepository
+    {
+        Task<bool> Create(Feature feature);
+        bool CheckFeatureByNameAndEnvironment(string name, string environment);
+    }
+}
