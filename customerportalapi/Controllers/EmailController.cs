bcfa31ad76ab57c6c05be6c2138f7968a38e3911
Christian Garcia PC-TPT2 <christian.garcia@quantion.com>
2021-05-10 17:21:43 +0200

    feat: EmaiLService

---
+++ customerportalapi/Controllers/EmailController.cs
@@ -0,0 +1,52 @@
+﻿using AutoWrapper.Wrappers;
+using customerportalapi.Entities;
+using customerportalapi.Security;
+using customerportalapi.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace customerportalapi.Controllers
+{
+    /// <summary>
+    /// Send email (use from internal webportal APIs)
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController
+    {
+        private readonly IEmailService _services;
+        private readonly ILogger<EmailController> _logger;
+
+
+        public EmailController(IEmailService services, ILogger<EmailController> logger)
+        {
+            _services = services;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [AuthorizeApiKey]
+        public async Task<ApiResponse> Send([FromBody] Email email)
+        {
+            try
+            {
+                var entity = await _services.Send(email);
+                return new ApiResponse(entity);
+            }
+            catch (Exception ex)
+            {
+                string obj = string.Empty;
+                if (email != null)
+                    obj = ", params:" + JsonConvert.SerializeObject(email);
+
+                _logger.LogError(ex, ex.Message + obj);
+                throw;
+            }
+        }
+    }
+}
