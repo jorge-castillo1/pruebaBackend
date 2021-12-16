688878065a8082f662c021fd79fd171cca3c581b
Andrea Garcia <andrea.garcia@quantion.com>
2021-12-16 07:56:55 +0000

    US25025 - [WP] LOGIN: solicitud de usuario

--- customerportalapi/Controllers/LoginController.cs
+++ customerportalapi/Controllers/LoginController.cs
@@ -105,30 +105,5 @@ namespace customerportalapi.Controllers
                 throw;
             }
         }
-
-        /// <summary>
-        /// Authenticate user against authenticator system
-        /// </summary>
-        /// <param name="value">Login credentials</param>
-        /// <returns>Access Token if succesfully logged</returns>
-        // POST api/login
-        [HttpPost]
-        public async Task<ApiResponse> PostNewUser([FromBody] Login value)
-        {
-            try
-            {
-                var entity = await _service.GetToken(value);
-                return new ApiResponse(entity);
-            }
-            catch (ServiceException se)
-            {
-                return new ApiResponse((int)se.StatusCode, new ApiError(se.Message, new[] { new ValidationError(se.Field, se.FieldMessage) }));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.ToString());
-                throw;
-            }
-        }
     }
 }
